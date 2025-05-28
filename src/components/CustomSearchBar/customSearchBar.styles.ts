3c1919c49dd745ede626d77b6a5dc090f153ee96
mobelite <ymbarek@mobelite.fr>
2025-05-28 18:26:42 +0100

    update search bar background color

--- src/components/CustomSearchBar/customSearchBar.styles.ts
+++ src/components/CustomSearchBar/customSearchBar.styles.ts
@@ -1,35 +1,35 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
-import { colors } from '_utils/colors';
-import { IS_IOS } from '_utils/constants';
-import { FontSizes } from '_utils/fonts';
-import { SEARCH_ICONS_DIMENTION } from '_utils/dimensions';
+import {colors} from '_utils/colors';
+import {IS_IOS} from '_utils/constants';
+import {FontSizes} from '_utils/fonts';
+import {SEARCH_ICONS_DIMENTION} from '_utils/dimensions';
 
 export default StyleSheet.create({
   container: {
-    backgroundColor: colors.WHITE_BACKGROUND,
+    backgroundColor: colors.LIGHT_GREY_BORDER,
     paddingHorizontal: 16,
     paddingVertical: IS_IOS ? 14 : 4,
     borderRadius: 22,
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 12
+    gap: 12,
   },
-  zeroOpacity: { opacity: 0 },
+  zeroOpacity: {opacity: 0},
   closeIcon: {
     width: SEARCH_ICONS_DIMENTION,
     height: SEARCH_ICONS_DIMENTION,
-    tintColor: colors.NEUTRAL_GREY
+    tintColor: colors.NEUTRAL_GREY,
   },
   textInput: {
     fontSize: FontSizes.FONT_SIZE_BASE_2,
     flex: 1,
     textAlignVertical: 'center',
-    color: colors.BLACK
+    color: colors.BLACK,
   },
   loopIcon: {
     width: SEARCH_ICONS_DIMENTION,
     height: SEARCH_ICONS_DIMENTION,
-    tintColor: colors.NEUTRAL_GREY
-  }
+    tintColor: colors.NEUTRAL_GREY,
+  },
 });
