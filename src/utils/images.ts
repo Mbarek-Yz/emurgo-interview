f2f9054fc1ecbb6bce62adac63b4b7e084338b9f
mobelite <ymbarek@mobelite.fr>
2025-05-28 18:25:13 +0100

    add default placeholder image

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,4 +2,5 @@ export const images = {
   LOOP: require('../assets/images/loop/loop.png'),
   CLOSE: require('../assets/images/close/close.png'),
   LOGIN: require('../assets/images/login/login.png'),
+  DEFAULT_PLACEHOLDER: require('../assets/images/default_placeholder/default_placeholder.png'),
 };
